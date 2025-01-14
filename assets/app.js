2f5ab8ee20e1a2be78ad5a2d8ea22b1c72ab3cb8
Thimea5 <thimea.hllv@gmail.com>
2025-01-14 13:10:28 +0100

    style

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,8 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 import './styles/menu.css';
+import './styles/connexion.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
