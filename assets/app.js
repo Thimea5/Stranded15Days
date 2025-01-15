9bfdc07b16b34f4a83c7c25cdecd447032f6d9f9
Thimea5 <thimea.hllv@gmail.com>
2025-01-15 16:43:03 +0100

    front connexion

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,31 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
 import './styles/menu.css';
-import './styles/connexion.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
+;
 
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener('DOMContentLoaded', function() {
+    //btn
+    let btnShowConnexion = document.getElementById('btnConnexion');
+    let btnAccueil = document.getElementById('btnAccueil');
+
+    //formulaires
+    let formConnexion = document.getElementById('form-connexion');
+
+    btnShowConnexion.addEventListener('click', function(){
+        event.preventDefault();
+        formConnexion.style.display = 'block';
+        document.getElementById("div-connexion").style.display = "block";
+    });
+
+    btnAccueil.addEventListener('click', function(){
+        event.preventDefault()
+        formConnexion.style.display = 'none';
+        document.getElementById("div-connexion").style.display = "none";
+    });
+});
