6eed5cc6b324f17dff22e7049e92909c170d4d23
Thimea5 <thimea.hllv@gmail.com>
2025-02-03 18:31:16 +0100

    Front accueil

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LastCall
 Projet Symfony du cours de PHP/JS
+
+
+Pour l'hébergement
+Always data (demander à Loïc)
--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,47 @@
 import './bootstrap.js';
 import './styles/app.css';
 import './styles/menu.css';
+import './styles/fonts.css';
 
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
-;
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+let tabForms= [];
 
 document.addEventListener('DOMContentLoaded', function() {
     //btn
     let btnShowConnexion = document.getElementById('btnConnexion');
+    let btnShowInscription = document.getElementById('btninscription');
     let btnAccueil = document.getElementById('btnAccueil');
 
     //formulaires
     let formConnexion = document.getElementById('form-connexion');
+    let formInscription = document.getElementById('form-inscription');
+    let divConnexion = document.getElementById('div-connexion')
+    tabForms.push(formConnexion);
+    tabForms.push(formInscription);
+    tabForms.push(divConnexion)
 
     btnShowConnexion.addEventListener('click', function(){
         event.preventDefault();
+        hideForms();
         formConnexion.style.display = 'block';
-        document.getElementById("div-connexion").style.display = "block";
+        divConnexion.style.display = "block";
     });
 
     btnAccueil.addEventListener('click', function(){
         event.preventDefault()
-        formConnexion.style.display = 'none';
-        document.getElementById("div-connexion").style.display = "none";
+        hideForms();
+    });
+
+    btnShowInscription.addEventListener('click', function(){
+        event.preventDefault();
+        hideForms();
+        formInscription.style.display = 'block';
+        document.getElementById("div-inscription ").style.display = "block";
     });
 });
+
+
+function hideForms(){
+    for (form of tabForms) {
+        form.style.display = "none";
+    }
+}
