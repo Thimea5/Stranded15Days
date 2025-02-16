58fb2f32c1f65ea685c621e673612146c75975b0
Thimea <thimea.hllv@gmail.com>
2025-02-16 19:35:58 +0100

    front

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,16 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 final class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'profil')]
-    public function index(): Response
-
+    public function index(SessionInterface $session): Response
     {
         //Ajouter sauvegarde avant la déconnexion
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'utilisateur' => $session->get('utilisateur')
         ]);
     }
 }
